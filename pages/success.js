811b84ba5065bc906d4597e58f7eeaf1c1cae3bc
fredxhansson-cmyk <fredxhansson@gmail.com>
2026-06-22 15:57:40 +0200

    add pages/success.js

---
+++ pages/success.js
@@ -0,0 +1,8 @@
+export default function Success() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Betalning Bekräftad</h1>
+      <p>Tack för ditt köp! Din prenumeration är nu aktiv.</p>
+    </div>
+  );
+}
